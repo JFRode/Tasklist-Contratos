98ff9ae2ecd871e9f68580fa8c24df853437accf
João Felipe Gonçalves <joaofelipe.rode@gmail.com>
2020-01-29 09:42:29 -0300

    CM - Adicionado TaskDto

---
+++ DataTransferObjects/TaskDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DataTransferObjects
+{
+    public class TaskDto
+    {
+        public Guid Id { get; set; }
+        public string Titulo { get; set; }
+        public bool Status { get; set; }
+        public string Descricao { get; set; }
+        public DateTime DataDeCriacao { get; set; }
+        public DateTime DataDeEdicao { get; set; }
+        public DateTime DataDeExclusao { get; set; }
+        public DateTime DataDeConclusao { get; set; }
+    }
+}
